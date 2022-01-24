57945ea1e93fc704262f077441e39fa8f402c036
nashitshayan <shayan.nashit@gmail.com>
2022-01-24 21:34:57 +0530

    practice

---
+++ Practice/practice.js
@@ -0,0 +1,38 @@
+// const proto = {
+//     hello(){
+//         return `Oui, my name is ${this.name}`;
+//     }
+// }
+
+
+// //const greeter =(name)=> Object.assign(Object.create(proto), {name});
+
+// //const nashit = greeter('nashit');
+
+
+// const newNash = Object.assign({}, proto, {name: 'Nashit'});
+// console.dir(newNash.hello())
+
+
+//module revealing pattern
+
+const myModule= (()=>{
+    'use strict'
+
+    let _privy = 'Hi there';
+    let publy= 'public ting';
+
+    let _priMethod = ()=>{
+        console.log(_privy);
+    }
+    let publyMethod = ()=>{
+        _priMethod();
+    }
+
+    return {publyMethod, publy};
+
+})();
+
+
+myModule.publyMethod();
+console.log(myModule.publy)
