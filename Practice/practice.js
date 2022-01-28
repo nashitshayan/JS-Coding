5f18a425cc4942b96c03fa3f64bbbfde7583f936
nashitshayan <shayan.nashit@gmail.com>
2022-01-28 21:31:02 +0530

    prac with classes

--- Practice/practice.js
+++ Practice/practice.js
@@ -38,10 +38,67 @@
 // console.log(myModule.publy)
 
 
-const aModule = (()=>{
-    const log= ()=> console.log('a module\'s log');
-    return {log}
-})();
+// const aModule = (()=>{
+//     const log= ()=> console.log('a module\'s log');
+//     return {log}
+// })();
 
-console.log(aModule)
-aModule.log();
+// console.log(aModule)
+// aModule.log();
+
+
+
+//class es6
+
+// class Man {
+//     constructor(name){
+//         this.name=name;
+//     }
+
+//     manHood(){
+//         console.log(`ABOUT TIME YOU BECAME A MAN ${this.name.toUpperCase()}!`);
+//     }
+// }
+
+// const boy= new Man('Nashit');
+
+// boy.manHood();
+
+// const Man= (name)=> {
+//     this.name= name;
+// }
+
+function Man(name) {
+    this.name = name;
+    function soo(){
+        console.log('ssooo')
+    }
+  }
+// Man.prototype.manHood = function() {
+//     console.log('hi')
+// };
+ 
+Man.prototype.sayHi = function() {
+    console.log(this.name);
+  };
+
+  const boy= new Man('Nashit');
+
+//boy.sayHi();
+
+for(let i in Man)
+    console.dir(i)
+
+
+/* 
+
+Three differences between class syntax and constructor syntax:
+
+1) A function that's created using class will have an internal property called [[isClassConstructor]]: true. The languages checks for this property in various places. For eg: A class function must always be called with 'new' keyword.
+
+2) Class methods are non-enumerable i.e the enumerable  flag is set to false for all methods in the 'prototype'.
+
+3) Classes always 'use strict'.
+
+
+*/
