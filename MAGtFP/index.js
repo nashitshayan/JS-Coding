d23533f39e29ba6ebebccc01fbcd377e9db8dbfb
Nashit Shayan <shayan.nashit@gmail.com>
2022-08-01 12:22:41 +0530

    Ch04 - ex2

--- MAGtFP/index.js
+++ MAGtFP/index.js
@@ -1,7 +1,7 @@
-import { split } from '@mostly-adequate/support';
+import { split, filter, match } from '@mostly-adequate/support';
 
-//Refactor to remove all arguments by partially applying the function.
-//ex1
+// Ex1 : Refactor to remove all arguments by partially applying the function.
+
 const words = (str) => split(' ', str);
 let q1 = words('I am a Frontend Developer');
 q1;
@@ -10,3 +10,14 @@ q1;
 const spliyBySpace = split(' ');
 let a1 = spliyBySpace('I am a Frontend Dev');
 a1;
+
+//Ex2: Refactor to remove all arguments by partially applying the functions.
+const filterQs = (xs) => filter((x) => match(/q/i, x), xs);
+let q2 = filterQs(['opaque', 'test', 'ThisHasAQInIt', 'Quirk', 'Plaque']);
+q2;
+
+//Sol2
+const matchQ = match(/q/i);
+const qFilter = filter(matchQ);
+let a2 = qFilter(['opaque', 'test', 'ThisHasAQInIt', 'Quirk', 'Plaque']);
+a2;
